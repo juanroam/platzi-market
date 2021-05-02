5957545ff46f2030e2f14949a7f6382ad3981d1a
Juan Carlos Rodríguez Amézquita <juanc.rodrigueza@hotmail.com>
2021-05-02 14:55:47 -0500

    Mapeo de las tablas en entidades.
    Dependencia para trabajar con base de datos PostreSQL.

---
+++ src/main/java/com/platzi/market/persistence/entity/Categoria.java
@@ -0,0 +1,42 @@
+package com.platzi.market.persistence.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private Long idCategoria;
+
+    private String descripcion;
+
+    private Boolean estado;
+
+    public Long getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Long idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
+}
