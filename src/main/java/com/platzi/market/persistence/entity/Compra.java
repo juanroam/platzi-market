4f23216a0cc9bb192206de91e3b550b372472010
Juan Carlos Rodríguez Amézquita <juanc.rodrigueza@hotmail.com>
2021-05-17 11:11:03 -0500

    Creación repositorio de compras y configuración cascade.

--- src/main/java/com/platzi/market/persistence/entity/Compra.java
+++ src/main/java/com/platzi/market/persistence/entity/Compra.java
@@ -29,7 +29,7 @@ public class Compra {
     @JoinColumn(name = "id_cliente", insertable = false, updatable = false)
     private Cliente cliente;
 
-    @OneToMany(mappedBy = "producto")
+    @OneToMany(mappedBy = "compra", cascade = {CascadeType.ALL})
     private List<ComprasProducto> productos;
 
     public Long getIdCompra() {
@@ -78,5 +78,13 @@ public class Compra {
 
     public void setEstado(String estado) {
         this.estado = estado;
+    }
+
+    public List<ComprasProducto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<ComprasProducto> productos) {
+        this.productos = productos;
     }
 }
